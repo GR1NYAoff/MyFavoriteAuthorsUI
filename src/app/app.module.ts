a076d0ef2bc5c6f5090e57f2e88fc019cacb84a0
= <=>
2022-06-27 00:47:09 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
+import { BookmarksListComponent } from './components/bookmarks-list/bookmarks-list.component';
+import { BookmarkComponent } from './components/bookmark/bookmark.component';
+import { SearchAuthorComponent } from './components/search-author/search-author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookmarksComponent,
+    BookmarksListComponent,
+    BookmarkComponent,
+    SearchAuthorComponent
   ],
   imports: [
     BrowserModule,
