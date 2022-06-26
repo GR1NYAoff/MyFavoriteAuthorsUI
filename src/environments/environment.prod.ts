44818d65b8337a1a40aa4b0e583e1630ad78f9d8
= <=>
2022-06-26 23:58:29 +0300

    added environment constants

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  authApi: 'http://localhost:5091/',
+  tokenDisallowedRoutes: ['http://localhost:5091/api/Account/login'],
+  tokenAllowedDomains: ['localhost:5091']
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  authApi: 'http://localhost:5091/',
+  tokenDisallowedRoutes: ['http://localhost:5091/api/Account/login'],
+  tokenAllowedDomains: ['localhost:5091']
 };
 
 /*
