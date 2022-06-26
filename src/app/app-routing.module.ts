9e10935d1a44fd7b911048b93b1777aeb4ebe402
= <=>
2022-06-27 01:02:29 +0300

    added base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchAuthorComponent } from './components/search-author/search-author.component';
+import { BookmarksListComponent } from './components/bookmarks-list/bookmarks-list.component';
+import { BookmarkComponent } from './components/bookmark/bookmark.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'bookmark', component: BookmarkComponent },
+  { path: 'bookmarks', component: BookmarksListComponent },
+  { path: 'search', component: SearchAuthorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
