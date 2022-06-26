77ecdf80b2f81f0f08cbd3864127e863589eb18a
= <=>
2022-06-27 00:50:09 +0300

    removed importing bookmarks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
 import { BookmarksListComponent } from './components/bookmarks-list/bookmarks-list.component';
 import { BookmarkComponent } from './components/bookmark/bookmark.component';
 import { SearchAuthorComponent } from './components/search-author/search-author.component';
@@ -13,7 +12,6 @@ import { SearchAuthorComponent } from './components/search-author/search-author.
   declarations: [
     AppComponent,
     HomeComponent,
-    BookmarksComponent,
     BookmarksListComponent,
     BookmarkComponent,
     SearchAuthorComponent
