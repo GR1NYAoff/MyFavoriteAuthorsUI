e854967c0f1474338ca618baed82b931be6d8d92
= <=>
2022-06-28 02:37:34 +0300

    added routing AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,22 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchAuthorComponent } from './components/search-author/search-author.component';
 import { BookmarksListComponent } from './components/bookmarks-list/bookmarks-list.component';
 import { BookmarkComponent } from './components/bookmark/bookmark.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'bookmarks', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'bookmark/:bookmarkId', component: BookmarkComponent },
-  { path: 'bookmarks', component: BookmarksListComponent },
+  {
+    path: 'bookmark/:bookmarkId',
+    component: BookmarkComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'bookmarks',
+    component: BookmarksListComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'search', component: SearchAuthorComponent },
 ];
 
